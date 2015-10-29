5cc10471e660feda27647f8e784726d7c1657d98
jeffward01 <jeffwardcoding@gmail.com>
2015-10-29 15:42:43 -0700

    built controllers

--- PropertyManager/PropertyManager.Core/Infrastructure/PropertyManagerDbContext.cs
+++ PropertyManager/PropertyManager.Core/Infrastructure/PropertyManagerDbContext.cs
@@ -19,5 +19,21 @@ namespace PropertyManager.Core.Infrastructure
         public IDbSet<Property> Properties { get; set; }
         public IDbSet<Tenant> Tenants { get; set; }
         public IDbSet<Lease> Leases { get; set; }
+
+
+        //Code First Fluid API
+        //Creates Database on these parameters
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Property>().HasKey(p => p.PropertyId);
+            modelBuilder.Entity<Property>().HasMany(p => p.Leases).WithRequired(l => l.Property).HasForeignKey(l => l.PropertyId);
+
+            modelBuilder.Entity<Tenant>().HasKey(t => t.TenantId);
+            modelBuilder.Entity<Tenant>().HasMany(l => l.Leases).WithRequired(l => l.Tenant).HasForeignKey(l => l.TenantId);
+
+            modelBuilder.Entity<Lease>().HasKey(l => l.LeaseId);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
---
+++ PropertyManager/PropertyManager/Models/LeaseModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyManager.Models
+{
+    class LeaseModel
+    {
+        public int LeaseId { get; set; }
+
+        public int PropertyId { get; set; }
+        public int TenantId { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public Nullable<DateTime> EndDate { get; set; }
+
+    }
+}
