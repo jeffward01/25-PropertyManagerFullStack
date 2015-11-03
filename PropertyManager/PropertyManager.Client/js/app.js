37ca42eeae31d283912b2750503209e94d34b23c
jeffward01 <jeffwardcoding@gmail.com>
2015-11-02 16:19:20 -0800

    wired up controllers

--- PropertyManager/PropertyManager.Client/js/app.js
+++ PropertyManager/PropertyManager.Client/js/app.js
@@ -6,17 +6,17 @@
         .state('dashboard', { url: '/dashboard', templateUrl: '/templates/dashboard/dashboard.html' })
 
 
-    //If state == property
+       //If state == property
         .state('property', { abstract: true, url: '/property', template: '<ui-view />' })
             .state('property.grid', { url: '/grid', templateUrl: '/templates/property/grid.html', controller: 'PropertyGridController' })
             .state('property.detail', { url: '/detail/:id', templateUrl: '/templates/property/detail.html', controller: 'PropertyDetailController' })
 
-    //If state == tenants
+       //If state == tenants
          .state('tenant', { abstract: true, url: '/tenant', template: '<ui-view />' })
             .state('tenant.grid', { url: '/grid', templateUrl: '/templates/tenant/grid.html', controller: 'TentantGridController' })
             .state('tenant.detail', { url: '/detail/:id', templateUrl: '/templates/tenant/detail.html', controller: 'TentantDetailController' })
 
-    //If state == lease
+       //If state == lease
         .state('lease', { abstract: true, url: '/lease', template: '<ui-view />' })
             .state('tenant.grid', { url: '/grid', templateUrl: '/templates/lease/grid.html', controller: 'LeaseGridController' })
             .state('tenant.detail', { url: '/detail/:id', templateUrl: '/templates/lease/detail.html', controller: 'LeaseDetailController' })
