f46ace5c9166673042e1693c598c780b861af820
jeffward01 <jeffwardcoding@gmail.com>
2015-11-05 17:05:57 -0800

    added validation for property controller:

--- PropertyManager/PropertyManager.Client/js/app/property/detail/property.detail.ctrl.js
+++ PropertyManager/PropertyManager.Client/js/app/property/detail/property.detail.ctrl.js
@@ -1,13 +1,23 @@
 angular.module('app').controller('PropertyDetailController', function ($scope, $stateParams, Property, $state) {
 
     if ($stateParams.id) {
-        $scope.property = Property.get({ id: $stateParams.id });
+        $scope.property = Property.get({
+            id: $stateParams.id
+        });
     } else {
         $scope.property = new Property();
     }
 
     $scope.saveProperty = function () {
+        //Validation
+        if (($('#propertyName').val() === "") || $(('#propertyAddress1').val() === "") || ($('#propertyCity').val() === "") || ($('#propertyState').val() === "") || ($('#propertyZip').val() === "")) {
+            alert("Please enter all required fields");
+            return false;
+        }
 
+
+
+        //Success Callback and Change of state
         var successfulCallback = function () {
             $state.go('app.property.grid');
         }
@@ -15,10 +25,9 @@ angular.module('app').controller('PropertyDetailController', function ($scope, $
 
         if ($scope.property.PropertyId) {
             $scope.property.$update(successfulCallback);
-            
+
         } else {
             $scope.property.$save(successfulCallback);
         };
     }
 });
-
